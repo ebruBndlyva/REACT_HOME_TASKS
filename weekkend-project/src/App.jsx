467193d24b6a3be76d0dd0e721ff9e0d9fbecf07
Ebru18 <ebrubendeliyeva@gmail.com>
2024-12-25 00:03:11 +0400

    update,delete,detail with db-json

--- weekkend-project/src/App.jsx
+++ weekkend-project/src/App.jsx
@@ -1,82 +1,175 @@
-import { useEffect, useState } from 'react'
-import { Space, Table,  Card } from 'antd';
+import { useEffect, useState } from 'react';
 import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
-import axios from "axios"
-import './App.css'
+import Table from 'react-bootstrap/Table';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from "axios";
+import './App.css';
+import Modal from 'react-bootstrap/Modal';
 
 function App() {
-  let [data, setData] = useState([])
-  const columns = [
-    {
-      title: 'Name',
-      dataIndex: 'name',
-
-      render: (text) => <a>{text}</a>,
-    },
-    {
-      title: 'Title',
-      dataIndex: 'title',
-
-    },
-    {
-      title: 'Email',
-      dataIndex: 'email',
-
-    },
-    {
-      title: 'Role',
-
-      dataIndex: 'role',
-
-    },
-    {
-      title: 'Action',
-
-      render: (_, record) => (
-        <Space size="middle">
-
-          <EditOutlined />
-          <DeleteOutlined onClick={()=>deleteData(record.key)}/>
-        </Space>
-      ),
-    },
-  ];
+  let [datas, setData] = useState([]);
+  let [modalData, setModalData] = useState({});
+  let [showModal, setShowModal] = useState(false);  // Modal üçün
+  let [showForm, setShowForm] = useState(false);    // Form üçün
+  let [updateData, setUpdateData] = useState({});
+  let [name, setName] = useState("");
+  let [prof, setProf] = useState("");
+  let [email, seteEmail] = useState("");
+  let [role, setRole] = useState("");
 
   function getData() {
     axios.get("http://localhost:4000/users")
       .then(res => {
-
-        const modifiedData = res.data.map(item => ({
-          ...item,
-          key: item.id,
-        }));
-        setData(modifiedData);
+        setData(res.data);
       });
   }
+
   async function deleteData(id) {
-   
-   try {
- 
-    await axios.delete("http://localhost:4000/users/" + id)
-    let filtered = data.filter((user) => user.id != id )
-    setData(filtered)
-   } catch (error) {
-    console.log(error);
-   }
-
-
-
+    try {
+      await axios.delete("http://localhost:4000/users/" + id);
+      let filtered = datas.filter((user) => user.id !== id);
+      setData(filtered);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
-  console.log(data);
+  function updatesData(id) {
+    axios.get("http://localhost:4000/users/" + id)
+      .then(res => {
+        setUpdateData(res.data);
+        setName(res.data.name);
+        setProf(res.data.profession);
+        seteEmail(res.data.email);
+        setRole(res.data.role);
+      });
+    setShowForm(true); // Formu aç
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    let newObj = {
+      id: updateData.id,
+      name: name,
+      profession: prof,
+      email: email,
+      role: role
+    };
+    axios.put(`http://localhost:4000/users/${updateData.id}`, newObj)
+      .then(res => {
+        console.log(res.data);
+        getData(); // Siyahını yenilə
+        setName("");
+        setProf("");
+        seteEmail("");
+        setRole("");
+        setShowForm(false); // Formu bağla
+      });
+  }
+
+  function handleModal(id) {
+    axios.get("http://localhost:4000/users/" + id)
+      .then(res => {
+        setModalData(res.data);
+      });
+    setShowModal(true); // Modalı aç
+  }
+
+  function closeForm() {
+    setShowForm(false); // Formu bağla
+  }
+
   useEffect(() => {
-    getData()
-  }, [])
+    getData();
+  }, []);
+
   return (
     <>
-      <Table columns={columns} dataSource={data} />
+      <Table striped bordered hover className='w-90% m-auto'>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Profession</th>
+            <th>Email</th>
+            <th>Role</th>
+            <th>Delete</th>
+            <th>Update</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            datas.map((user) => (
+              <tr key={user.id}>
+                <td>{user.id}</td>
+                <td style={{cursor:'pointer'}} onClick={() => handleModal(user.id)}>{user.name}</td>
+                <td>{user.profession}</td>
+                <td>{user.email}</td>
+                <td>{user.role}</td>
+                <td>
+                  <DeleteOutlined className='btn btn-danger' onClick={() => deleteData(user.id)} />
+                </td>
+                <td>
+                  <EditOutlined className='btn btn-primary' onClick={() => updatesData(user.id)} />
+                </td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </Table>
+
+      {/* Modal */}
+      <div
+        className="modal show"
+        style={{ display: showModal ? "block" : "none" }}
+      >
+        {
+          <Modal.Dialog key={modalData.id}>
+            <Modal.Header className='d-flex justify-content-between'>
+              <Modal.Title style={{ textTransform: "uppercase" }}>{modalData.name}</Modal.Title>
+              <button onClick={() => setShowModal(false)} className='btn btn-warning'>X</button>
+            </Modal.Header>
+
+            <Modal.Body>
+              <ol>
+                <li>{modalData.profession}</li>
+                <li>{modalData.email}</li>
+                <li>{modalData.role}</li>
+              </ol>
+            </Modal.Body>
+          </Modal.Dialog>
+        }
+      </div>
+
+      <div
+        style={{ display: showModal ? "block" : "none" }}
+        className="overlay"
+      ></div>
+
+      {/* Form və Overlay */}
+      <div className="form-container" style={{ display: showForm ? "block" : "none" }}>
+        <div className="overlay-form" onClick={closeForm}></div>  {/* Overlay */}
+        <div className="update-form">
+          <form onSubmit={handleSubmit} key={updateData.id}>
+            <label htmlFor="name">Ad:</label>
+            <input id="name" value={name} onChange={(e) => setName(e.target.value)} type="text" />
+
+            <label htmlFor="prof">Peşə:</label>
+            <input id="prof" value={prof} onChange={(e) => setProf(e.target.value)} type="text" />
+
+            <label htmlFor="email">E-poçt:</label>
+            <input id="email" value={email} onChange={(e) => seteEmail(e.target.value)} type="email" />
+
+            <label htmlFor="role">Rol:</label>
+            <input id="role" value={role} onChange={(e) => setRole(e.target.value)} type="text" />
+
+            <button className='btn btn-success' type="submit">Update</button>
+            <button className='btn btn-danger' type="button" onClick={closeForm}>X</button> {/* Close button */}
+          </form>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
